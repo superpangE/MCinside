419ead8552178723c7a47deb1ed8830666652331
superpangE <cdpkct@gmail.com>
2020-12-16 22:57:15 +0900

    검색페이지 시작

---
+++ be/public/components/BoardHead/index.js
@@ -0,0 +1,34 @@
+import { getLabel, getButton, getContainer } from '../common.js';
+
+const BoardHead = () => {
+  const MainContainer = getContainer(null, 'header_main');
+  const BtnWrap = getContainer(null, 'header_btnwrap');
+  const TitleWrap = getContainer(null, 'header_titlewrap');
+  const SetupWrap = getContainer(null, 'header_settingwrap');
+  const MainTitle = getLabel('header_title', '리그 오브 레전드 갤러리');
+  const SimilGal = getLabel('header_setup', '연관 갤러리');
+  const GalCopy = getLabel('header_setup', '갤주소 복사');
+  const GalBan = getLabel('header_setup', '차단설정');
+  const GalUse = getLabel('header_setup', '갤러리 이용안내');
+  const AllBoard = getButton('all_board', '전체글');
+  const GoodBoard = getButton('good_board', '개념글');
+  const NoticeBoard = getButton('notice_board', '공지');
+
+  BtnWrap.insertAdjacentElement('beforeend', AllBoard);
+  BtnWrap.insertAdjacentElement('beforeend', GoodBoard);
+  BtnWrap.insertAdjacentElement('beforeend', NoticeBoard);
+
+  SetupWrap.insertAdjacentElement('beforeend', SimilGal);
+  SetupWrap.insertAdjacentElement('beforeend', GalCopy);
+  SetupWrap.insertAdjacentElement('beforeend', GalBan);
+  SetupWrap.insertAdjacentElement('beforeend', GalUse);
+
+  TitleWrap.insertAdjacentElement('beforeend', MainTitle);
+  TitleWrap.insertAdjacentElement('beforeend', SetupWrap);
+
+  MainContainer.insertAdjacentElement('beforeend', TitleWrap);
+  MainContainer.insertAdjacentElement('beforeend', BtnWrap);
+
+  return MainContainer;
+};
+export default BoardHead;
--- be/public/index.js
+++ be/public/index.js
@@ -1,4 +1,5 @@
 import MainPage from './Page/index.js';
+import BoardPage from './Page/boardpage.js';
 // 문자열 html -> 엘리먼트
 
 const Root = async () => {
@@ -7,4 +8,10 @@ const Root = async () => {
   root.insertAdjacentElement('beforeend', Main);
 };
 
-Root();
+const Root2 = () => {
+  const root = document.querySelector('#root');
+  const Board = BoardPage();
+  root.insertAdjacentElement('beforeend', Board);
+};
+
+Root2();
